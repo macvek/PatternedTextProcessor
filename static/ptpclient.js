5c36aeaa32f316654923aacc7a1816fa37398745
Maciej Aleksandrowicz <macvek@gmail.com>
2021-03-04 19:13:57 +0100

    sample form with submit

--- static/ptpclient.js
+++ static/ptpclient.js
@@ -5,8 +5,66 @@ window.addEventListener('load', (e) => {
 async function startPTP() {
     const hello = await Comm.send('/hello', {});
     const root = document.getElementById('root');
-    root.innerText = `Got version ${hello.version}`;
+    let versionBox = document.createElement('div');
+    versionBox.classList.add('version-box');
+    versionBox.innerText = `Got version ${hello.version}`;
+    root.innerHTML = '';
+    root.appendChild(versionBox);
+
+    let ui = new UI();
+    let [sampler, samplerControl] = ui.namedInput('Sampler');
+    root.appendChild(sampler);
+
+    let [sendSample, sendSampleCtl] = ui.submitButton('Check sample', samplerControl.input);
+    root.appendChild(sendSample);
 }
+
+class UI {
+    submitButton(name, triggerFromInput) {
+        let button = document.createElement('button');
+        button.innerText = name;
+        
+        let control = {
+            callClick() {
+                console.log('clicked');
+            }
+        }
+        
+        button.onclick = () => { control.callClick();}
+        if (triggerFromInput) {
+            this.bindToEnterPress(triggerFromInput, () => {
+                control.callClick();
+            });
+        }
+
+        return [button, control];
+    }
+
+    namedInput(name) {
+        let box = document.createElement('span');
+        let label = document.createElement('label');
+        label.innerText = name;
+        box.appendChild(label);
+
+        let input = document.createElement('input');
+        box.appendChild(input);
+
+        let control = {
+            value() { return input.value; },
+            input
+        }
+        return [box, control];
+    }
+
+    bindToEnterPress(what, callback) {
+        what.addEventListener('keypress', (e) => {
+            if (e.key === 'Enter') {
+                callback(e);
+            }
+        })
+    }
+}
+
 
 class Comm {
     static async send(endpoint, payload) {
